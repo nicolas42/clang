17dc042b8ae8ce6e9f8eeef60b20ac6f2125eef3
Nicolas Schmidt <nicolas42@gmail.com>
2019-09-23 14:22:20 +1000

    tests: static, embedded structs, file read and parse

--- globals_test/main.c
+++ globals_test/main.c
@@ -2,6 +2,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 #include "mymath.h"
 
 
@@ -22,27 +23,66 @@ static int x; // file scope
 
 char* read_file(FILE* file) {
 
-    int length = 100;
-    char* data = malloc(sizeof(char) * length);
-    int index = 0;
-    int value = 0;
+  int length = 100;
+  char* data = malloc(sizeof(char) * length);
+  int index = 0;
+  int value = 0;
 
-    while (1) {
+  while (1) {
 
-        if (index == length) {
-            length = length * 2;
-            data = realloc(data, sizeof(char) * length);
-        }
+      if (index == length) {
+          length = length * 2;
+          data = realloc(data, sizeof(char) * length);
+      }
 
-        value = fgetc(file);
-        if (value == EOF) {
-            data[index] = '\0';
-            return data;
-        } else {
-            data[index] = (char)value;
-            index += 1;
-        }
+      value = fgetc(file);
+      if (value == EOF) {
+          data[index] = '\0';
+          return data;
+      } else {
+          data[index] = (char)value;
+          index += 1;
+      }
+  }
+}
+
+int* parse_string(char* arg, int* length_arg) {
+
+  int length = *length_arg;
+  int* offsets = malloc(sizeof(int) * length);
+  int index = 0;
+  int is_in_word = 0;
+  
+  for (int i = 0; arg[i] != '\0'; i += 1){
+
+    if (i == length) {
+        length = length * 2;
+        offsets = realloc(offsets, sizeof(char) * length);
     }
+
+    // if the value under the input index is a non-whitespace character 
+    // and the previous char was a whitespace char OR we are at the head of the input
+    // then note down the beginning offset of the word
+
+
+    if ( (!isspace(arg[i])) && (i == 0 || isspace(arg[i-1])) ){
+      offsets[index] = i;
+      index++;
+      is_in_word = 1;
+    }
+    if ( (i != 0) && ( (isspace(arg[i]) || arg[i] == '\0') && (!isspace(arg[i-1])) ) ) {
+      offsets[index] = i;
+      index++;
+      is_in_word = 0;
+    }
+
+    if ( isspace(arg[i]) ){
+      arg[i] = '\0';
+    }
+
+  }
+  *length_arg = index;
+  return offsets;
 }
 
 
@@ -61,8 +101,17 @@ int main()
 
     file = fopen("record", "r");
     string = read_file(file);
-    
-    printf("%s", string);
+
+    int length = 100;
+    int* offsets = parse_string(string, &length);
+
+    printf("%s", string + offsets[0]);
+    printf("%s", string + offsets[2]);
+    printf("%s", string + offsets[4]);
+    printf("%s", string + offsets[6]);
+    printf("%s", string + offsets[8]);
+
+    // printf("%s", string);
     fclose(file);  
   }
 
@@ -70,3 +119,22 @@ int main()
 }
 
 
+
+// struct Bytes {
+//   int length;
+//   char* offsets;
+//   int index;
+//   int is_in_word;
+// };
+
+// int bytes_init(struct Bytes* arg){
+
+//   arg->length = 100;
+//   arg->offsets = malloc(sizeof(char) * arg->length);
+//   arg->index = 0;
+//   arg->is_in_word = 0;
+
+// }
+
+  // Bytes offsets;
+  // bytes_init(&offsets);
