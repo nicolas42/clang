26939380d99a41b25f6c3cd8988bd6d156a0cdf5
Nicolas Schmidt <nicolas42@gmail.com>
2019-09-25 04:25:47 +1000

    geometric product

--- multivector.c
+++ multivector.c
@@ -1,58 +1,96 @@
 // gcc -Wall -pedantic multivector.c && ./a.out
 
+// https://marctenbosch.com/quaternions/
+
 #include <stdio.h>
 #include <stdlib.h>
 
-void geometric_product(int *c, int *a, int *b){
+#define mul(c,a,b) geometric_product(c,a,b)
+
+void geometric_product(double *c, double *a, double *b){
+	// I've probably made a mistake somewhere o_o
 	c[0] = a[0]*b[0] + a[1]*b[1] + a[2]*b[2] + a[3]*b[3] - a[4]*b[4] - a[5]*b[5] - a[6]*b[6] - a[7]*b[7];
-	c[1] = a[0]*b[2] + a[2]*b[0] - a[2]*b[6] + a[6]*b[2] + a[3]*b[5] - a[5]*b[3] + a[7]*b[4] + a[4]*b[7];
+
+	c[1] = a[0]*b[1] + a[1]*b[0] - a[2]*b[6] + a[6]*b[2] + a[3]*b[5] - a[5]*b[3] + a[7]*b[4] + a[4]*b[7];
 	c[2] = a[0]*b[2] + a[2]*b[0] + a[1]*b[6] - a[6]*b[1] - a[3]*b[4] + a[4]*b[3] - a[7]*b[5] - a[5]*b[7];
-	c[3] = a[0]*b[3] + a[3]*b[0] - a[1]*b[5] + a[5]*b[1] + a[2]*b[4] - a[4]*b[3] + a[7]*b[4] + a[4]*b[7];
+	c[3] = a[0]*b[3] + a[3]*b[0] - a[1]*b[5] + a[5]*b[1] + a[2]*b[4] - a[4]*b[3] + a[7]*b[6] + a[6]*b[7];
+
 	c[4] = a[4]*b[0] + a[0]*b[4] + a[2]*b[3] - a[3]*b[2] + a[6]*b[5] - a[5]*b[6] + a[7]*b[1] + a[1]*b[7];
 	c[5] = a[5]*b[0] + a[0]*b[5] + a[3]*b[1] - a[1]*b[3] + a[4]*b[6] - a[6]*b[4] + a[7]*b[2] + a[2]*b[7];
 	c[6] = a[6]*b[0] + a[0]*b[6] + a[1]*b[2] - a[2]*b[1] + a[5]*b[4] - a[4]*b[5] + a[7]*b[3] + a[3]*b[7];
+
 	c[7] = a[0]*b[7] + a[7]*b[0] + a[1]*b[4] + a[4]*b[1] + a[2]*b[5] + a[5]*b[2] + a[3]*b[6] + a[6]*b[3];
 }
+
+
+void geometric_print(double *a){
+	printf("[%.0f %.0f %.0f %.0f %.0f %.0f %.0f %.0f]", a[0], a[1], a[2], a[3], a[4], a[5], a[6], a[7]);
+}
+
+// void geometric_add(double *c, double *a, double *b){
+// 	for (int i = 0; i < 8; i += 1){
+// 		c[i] = a[i] + b[i];
+// 	}
+// }
+
+// void geometric_negate(double *c, double *a){
+// 	for (int i = 0; i < 8; i += 1){
+// 		c[i] = - a[i];
+// 	}
+// }
 
 // Test Multiplying two 3 dimensional multivectors
 // https://www.euclideanspace.com/maths/algebra/clifford/d3/arithmetic/index.htm
 int main(int argc, char **argv)
 {
-	// scalar, vector3, bivector3, pseudoscalar
-	// bivector has components e23 e31 e12 
-	// pseudoscalar has component e123
-	// double a[8] = {1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0};
-	// double b[8] = {1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0};
+/*
 	double a[8] = {1, 0, 0, 0, 1, 0, 0, 0};
 	double b[8] = {1, 0, 0, 0, 1, 0, 0, 0};
 	double c[8] = {0, 0, 0, 0, 0, 0, 0, 0};
 
 	geometric_product(c,a,b);
-	// c[0] = a[0]*b[0] + a[1]*b[1] + a[2]*b[2] + a[3]*b[3] - a[4]*b[4] - a[5]*b[5] - a[6]*b[6] - a[7]*b[7];
-	// c[1] = a[0]*b[2] + a[2]*b[0] - a[2]*b[6] + a[6]*b[2] + a[3]*b[5] - a[5]*b[3] + a[7]*b[4] + a[4]*b[7];
-	// c[2] = a[0]*b[2] + a[2]*b[0] + a[1]*b[6] - a[6]*b[1] - a[3]*b[4] + a[4]*b[3] - a[7]*b[5] - a[5]*b[7];
-	// c[3] = a[0]*b[3] + a[3]*b[0] - a[1]*b[5] + a[5]*b[1] + a[2]*b[4] - a[4]*b[3] + a[7]*b[4] + a[4]*b[7];
-	// c[4] = a[4]*b[0] + a[0]*b[4] + a[2]*b[3] - a[3]*b[2] + a[6]*b[5] - a[5]*b[6] + a[7]*b[1] + a[1]*b[7];
-	// c[5] = a[5]*b[0] + a[0]*b[5] + a[3]*b[1] - a[1]*b[3] + a[4]*b[6] - a[6]*b[4] + a[7]*b[2] + a[2]*b[7];
-	// c[6] = a[6]*b[0] + a[0]*b[6] + a[1]*b[2] - a[2]*b[1] + a[5]*b[4] - a[4]*b[5] + a[7]*b[3] + a[3]*b[7];
-	// c[7] = a[0]*b[7] + a[7]*b[0] + a[1]*b[4] + a[4]*b[1] + a[2]*b[5] + a[5]*b[2] + a[3]*b[6] + a[6]*b[3];
 
-	printf("Geometric product of\n");
-	printf("[%.1f %.1f %.1f %.1f %.1f %.1f %.1f %.1f]\n", a[0], a[1], a[2], a[3], a[4], a[5], a[6], a[7]);
-	printf("[%.1f %.1f %.1f %.1f %.1f %.1f %.1f %.1f]\n", b[0], b[1], b[2], b[3], b[4], b[5], b[6], b[7]);
-	printf("[%.1f %.1f %.1f %.1f %.1f %.1f %.1f %.1f]\n", c[0], c[1], c[2], c[3], c[4], c[5], c[6], c[7]);
+	printf("// Test Multiplying two 3 dimensional multivectors\n");
+	geometric_print(a); printf("\n");
+	geometric_print(b); printf("\n");
+	geometric_print(c); printf("\n");
+*/	
 
-	// c0=a0b0+a1b1+a2b2+a3b3+a4b4+a5b5+a6b6+a7b7
-	// c1=a0b1+a1b0-a2b6+a6b2+a3b5-a5b3+a7b4+a4b7
-	// c2=a0b2+a2b0+a1b6-a6b1-a3b4+a4b3-a7b5-a5b7
-	// c3=a0b3+a3b0-a1b5+a5b1+a2b4-a4b3+a7b4+a4b7
-	// c4=a4b0+a0b4+a2b3-a3b2+a6b5-a5b6+a7b1+a1b7
-	// c5=a5b0+a0b5+a3b1-a1b3+a4b6-a6b4+a7b2+a2b7
-	// c6=a6b0+a0b6+a1b2-a2b1+a5b4-a4b5+a7b3+a3b7
-	// c7=a0b7+a7b0+a1b4+a4b1+a2b5+a5b2+a3b6+a6b3
+	// make_vector(&v, 1,0,0);
+	// make_bivector(&v, 1,0,0);
+	// make_complex(&v,1,1);
+	// make_quaternion(&v,1,2,3,4);
+	// make_scalar(&v,1);
+	// make_four_vector(&v,1,2,3,4);
+
+	double v[8] = {0, 1, 0, 0, 0, 0, 0, 0};
+	double a[8] = {0, 1, 0, 0, 0, 0, 0, 0};
+	double b[8] = {0, 1, 1, 0, 0, 0, 0, 0};
+
+	double ab[8] = {0, 0, 0, 0, 0, 0, 0, 0};
+	double ba[8] = {0, 0, 0, 0, 0, 0, 0, 0};
+	double v2[8] = {0, 0, 0, 0, 0, 0, 0, 0};
+	double Rab[8] = {0, 0, 0, 0, 0, 0, 0, 0};
+
+	mul(ab, a, b);
+	mul(ba, b, a);
+
+	mul(v2, v, ab);
+	mul(Rab, ba, v2);
+
+
+	printf("// Test Multiplying two 3 dimensional multivectors\n");
+	geometric_print(v); printf("\n");
+	geometric_print(a); printf("\n");
+	geometric_print(b); printf("\n");
+	geometric_print(Rab); printf("\n");
 
 	return 0;
 }
+
+
+
+
 // int main(int argc, char **argv)
 // {
 // 	// Multiply two multivectors a and b to make c
