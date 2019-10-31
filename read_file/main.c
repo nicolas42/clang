b9b8a842d0666fe7f5513ad40aed8143f89a2fae
nicolas42 <nicolas42@gmail.com>
2019-11-01 08:45:06 +1000

    test

--- read_file/main.c
+++ read_file/main.c
@@ -1,10 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <stdint.h>
+
+int read_file1(char* filename)
+{
+        char c = '\0';
+        FILE* file = fopen(filename,"r");
+
+        while(1){
+                c = fgetc(file);
+                if (c == EOF){
+                        break;
+                } else {
+                        putc(c, stdout);
+
+                }
+        }
+
+        return 0;
+}
 
 
 int main(int argc, char **argv)
 {
-	printf("Hello World\r\n");
-	return 0;
+        printf("Hello World\r\n");
+        read_file1("main.c");
+        return 0;
 }
+
