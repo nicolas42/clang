ad9cf917bf57d60f83742c0d5053b34e7a10cb98
Nicolas Schmidt <nicolas42@gmail.com>
2020-04-10 23:56:34 +1000

    cpp version added

--- arena_allocator/multivector.cpp
+++ arena_allocator/multivector.cpp
@@ -1,11 +1,15 @@
-// clang -std=c11 -Weverything multivector.c; ./a.out
+// clang -std=c++11 -Weverything multivector.cpp; ./a.out
 
+extern "C" {
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 #include <stdarg.h>
 
 // #include "short_type.h"
+}
+
+namespace multivector {
 
 typedef double* multivector_t;
 #define multivector_length 8
@@ -42,7 +46,7 @@ multivector_t arena_allocate(size_t len)
     // realloc on overflow
     if (arena_length + len > arena_capacity){
         arena_capacity *= 2;
-        arena = realloc(arena, arena_capacity * sizeof(multivector_t));
+        arena = multivector_t(realloc(arena, arena_capacity * sizeof(multivector_t)));
     }
 
     arena_length += len;
@@ -207,14 +211,18 @@ void print_trivector(multivector_t a)
     printf( "trivector( %.3f )\n", a[7]);
 }
 
+}
+
 int main(void)
 {
+    using namespace multivector;
+    
     // #include "multiline_string_literal.h"
     // printf("%s", multiline_string_literal);
 
     #define T 6.28318530718
 
-    arena = malloc(arena_capacity * sizeof(multivector_t));
+    arena = multivector_t(malloc(arena_capacity * sizeof(multivector_t)));
 
     multivector_t v = vector(1,0,0);
     multivector_t a = vector(1,0,0);
@@ -222,60 +230,60 @@ int main(void)
     // multivector_t c;
     multivector_t r;
     multivector_t spinor;
-    printf("\n");
-    printf("length\n");
+    
+    printf("\nlength\n");
     printf("multiplying two vectors which have the same direction is equivalent to dotting them together\n");
     printf("which gives the length squared\n");
-    printf("r = mul(2, vector(1,1,0), vector(1,1,0));\n");
+    printf("r = mul(2, vector(1,1,0), vector(1,1,0));\n");    
     r = mul(2, vector(1,1,0), vector(1,1,0));
     print_scalar(r);
 
-    printf("\n");
-    printf("In spherical coordinates (r,theta,phi) where T = 2*Pi\n");
+    printf("\nIn spherical coordinates (r,theta,phi) where T = 2*Pi\n");
     printf("r = mul(2, vector_spherical(1, T/8, T/4), vector_spherical(1, T/8, T/4));\n");
     r = mul(2, vector_spherical(1, T/8, T/4), vector_spherical(1, T/8, T/4));
     print_scalar(r);
-    printf("\n");
-    printf("Area\n");
+
+    printf("\nArea\n");
     printf("Multiplying two vectors which are orthogonal to each other will give the area of the rectangle\n");
     printf("r = mul(2, vector(1,0,0), vector(0,1,0));\n");
     r = mul(2, vector(1,0,0), vector(0,1,0));
     print_bivector(r);
-    printf("\n");
-    printf("Here we have two vectors in spherical coordinates.\n");
+
+    printf("\nHere we have two vectors in spherical coordinates.\n");
     printf("Multiplying them gives the dot product and the wedge product (similar to the cross product.\n");
     printf("r = mul(2, vector_spherical(1, T/8, T/8), vector_spherical(1, T/4, T/4));\n");
     r = mul(2, vector_spherical(1, T/8, T/8), vector_spherical(1, T/4, T/4));
     print(r);
-    printf("\n");
-    printf("volume\n");
+
+    printf("\nvolume\n");
     printf("Volume works great but you get a trivector\n");
     printf("r = mul(3, vector(1,0,0), vector(0,1,0), vector(0,0,1));\n");
     r = mul(3, vector(1,0,0), vector(0,1,0), vector(0,0,1));
     print_trivector(r);
-    printf("\n");
+
     printf("r = mul(3, vector_spherical(1, T/8, T/4), vector_spherical(1, 3*T/8, T/4), vector(0,0,1));\n");
     r = mul(3, vector_spherical(1, T/8, T/4), vector_spherical(1, 3*T/8, T/4), vector(0,0,1));
     print_trivector(r);
+
     printf("A trivector is also known as a pseudoscalar since it's a single number\n");
     printf("but it squares to give a negative scalar\n");
-    printf("\n");
-    printf("\n");
-    printf("2D rotation (complex numbers)\n");
+
+
+    printf("\n2D rotation (complex numbers)\n");
     printf("A vector can be rotated by multiplying it by a bivector\n");
     printf("A bivector works like a complex number\n");
     printf("r = mul(2, vector(1,0,0), bivector(1,0,0)); \n");
     r = mul(2, vector(1,0,0), bivector(1,0,0)); 
     print_vector(r);
-    printf("\n");
+
     printf("spinor = mul(2, vector(1,0,0), vector_spherical(1, T/12, T/4));\n");
     printf("r = mul(2, vector(1,0,0), spinor);\n");
     spinor = mul(2, vector(1,0,0), vector_spherical(1, T/12, T/4));
     r = mul(2, vector(1,0,0), spinor);
     print_vector(r);
-    printf("\n");
-    printf("\n");
-    printf("3D rotation work in this way apparently\n");
+
+
+    printf("\n3D rotation work in this way apparently\n");
     printf("To rotate a vector 'v' in the arc from vector 'a' to vector 'b'\n");
     printf("multiply(b,a,v,a,b);\n");
     printf("This will rotate v by twice the angle between a and b\n");
@@ -289,8 +297,8 @@ int main(void)
     b = vector_spherical(1, T/8, T/8);
     r = mul(5, b,a,v,a,b);
     print_vector(r);
-    printf("\n");
-    printf("Does it work just multiplying on one side? Not generally I think.\n");
+
+    printf("\nDoes it work just multiplying on one side? Not generally I think.\n");
     printf("spinor1 = mul(2, vector(1,0,0), vector_spherical(1,T/8,T/4))\n");
     printf("spinor2 = mul(2, vector(1,0,0), vector_spherical(1,T/8,T/4))\n");
     printf("mul(3, vector(1,0,0), spinor1, spinor2);\n");
@@ -298,8 +306,8 @@ int main(void)
     multivector_t spinor1 = mul(2, vector(1,0,0), vector_spherical(1,T/8,T/4));
     multivector_t spinor2 = mul(2, vector(1,0,0), vector_spherical(1,T/8,T/4));
     mul(3, vector(1,0,0), spinor1, spinor2);
-    printf("\n");
-    printf("chirality???\n");
+
+    printf("\nchirality???\n");
     printf("r = mul(2, trivector(3), trivector(4));\n");
     r = mul(2, trivector(3), trivector(4));
     print(r);
